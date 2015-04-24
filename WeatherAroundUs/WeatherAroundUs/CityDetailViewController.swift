bce950d4d0dbfac784ad60fd064d13881e5817d6
Yu Wang <wangyu2165@gmail.com>
2015-04-24 00:43:02 -0500

    refactor structure and cache

--- WeatherAroundUs/WeatherAroundUs/CityDetailViewController.swift
+++ WeatherAroundUs/WeatherAroundUs/CityDetailViewController.swift
@@ -2,48 +2,28 @@
 //  CityDetailViewController.swift
 //  WeatherAroundUs
 //
-//  Created by Kedan Li on 15/4/4.
-//  Copyright (c) 2015年 Kedan Li. All rights reserved.
+//  Created by Wang Yu on 4/23/15.
+//  Copyright (c) 2015 Kedan Li. All rights reserved.
 //
 
 import UIKit
-import GPUImage
+import Spring
 
 class CityDetailViewController: UIViewController {
 
+    @IBOutlet var backgroundImageView: DesignableImageView!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var mainTemperatureDisplay: UILabel!
+    @IBOutlet var degreeToTopHeightConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var image = GPUImagePicture(image: UIImage(named: "20150104075606381.jpg")!)
-        var filter = GPUImageToonFilter()
-        filter.threshold = 0.8
-        filter.quantizationLevels = 40
-
-        var img = filter.imageByFilteringImage(UIImage(named: "1175782717.jpg"))
-        
-        var imgv = UIImageView(image: img)
-        imgv.frame = CGRectMake(10, 10, 620, 400)
-        
-        self.view.addSubview(imgv)
-
-        
-        // Do any additional setup after loading the view.
+        degreeToTopHeightConstraint.constant = view.frame.height / 4
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
