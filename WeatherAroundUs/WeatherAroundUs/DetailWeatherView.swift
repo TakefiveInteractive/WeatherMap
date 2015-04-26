f07988d4312e9f1df9a1400acde181a661ce69af
Yu Wang <wangyu2165@gmail.com>
2015-04-25 20:28:19 -0500

    minor update

--- WeatherAroundUs/WeatherAroundUs/DetailWeatherView.swift
+++ WeatherAroundUs/WeatherAroundUs/DetailWeatherView.swift
@@ -40,27 +40,34 @@ class DetailWeatherView: UIView {
             unit = "C"
         }
         createTwoUILabelInMiddle("Wind Speed:", secondString: "\(windSpeed) mps", yPosition: beginY)
+        
         beginY += blockHeight
         createTwoUILabelInMiddle("Wind Direction:", secondString: windDirectionStr, yPosition: beginY)
+        
         beginY += blockHeight + spaceHeight
         let clouds = (forecastInfos[0] as [String: AnyObject])["speed"] as! Int
         createTwoUILabelInMiddle("Cloudiness:", secondString: "\(clouds) %", yPosition: beginY)
+        
         beginY += blockHeight
-        let chanceOfRain = (forecastInfos[0] as [String: AnyObject])["rain"] as? Double
-        var chanceOfRainStr = "Yet updated"
-        if chanceOfRain != nil {
-            chanceOfRainStr = "\(chanceOfRain! * 100) %"
-        }
-        createTwoUILabelInMiddle("Chance of Rain:", secondString: chanceOfRainStr, yPosition: beginY)
-        beginY += blockHeight + spaceHeight
         let humanity = (forecastInfos[0] as [String: AnyObject])["humidity"] as! Int
         createTwoUILabelInMiddle("Humanity:", secondString: "\(humanity) %", yPosition: beginY)
+        
+        beginY += blockHeight + spaceHeight
+        let precipitation = (forecastInfos[0] as [String: AnyObject])["rain"] as? Double
+        var precipitationStr = "TBA"
+        if precipitation != nil {
+            precipitationStr = "\(precipitation! * 100) mm"
+        }
+        createTwoUILabelInMiddle("Precipitation:", secondString: precipitationStr, yPosition: beginY)
+        
         beginY += blockHeight
         let pressure = (forecastInfos[0] as [String: AnyObject])["pressure"] as! Int
         createTwoUILabelInMiddle("Pressure:", secondString: "\(pressure) hPa", yPosition: beginY)
+        
         beginY += blockHeight + spaceHeight
         let mornTemperature = ((forecastInfos[0]["temp"] as! [String: AnyObject])["morn"])!.intValue
         createTwoUILabelInMiddle("Morn Temp:", secondString: "\(parentController.degreeConvert(mornTemperature)) °" + unit, yPosition: beginY)
+        
         beginY += blockHeight
         let nightTemperature = ((forecastInfos[0]["temp"] as! [String: AnyObject])["night"])!.intValue
         createTwoUILabelInMiddle("Niehgt Temp:", secondString: "\(parentController.degreeConvert(nightTemperature)) °" + unit, yPosition: beginY)
