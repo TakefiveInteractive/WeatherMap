96d8ce51665284bdf551d9157f409aa8aff937db
Yu Wang <wangyu2165@gmail.com>
2015-06-13 19:27:38 +0800

    handle eventbrite api

---
+++ WeatherAroundUs/WeatherAroundUs/EventbriteGetter.swift
@@ -0,0 +1,13 @@
+//
+//  EventbriteGetter.swift
+//  WeatherMap
+//
+//  Created by Wang Yu on 6/13/15.
+//  Copyright (c) 2015 Kedan Li. All rights reserved.
+//
+
+import UIKit
+
+class EventbriteGetter: NSObject {
+   
+}
